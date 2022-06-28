7ad1f018b8682c0e6f03d3727a27836c92c071cb
Bushido-brown <orizut@gmail.com>
2022-06-28 10:59:42 -0700

    add math magicians components

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className='container'>
+      <div className="container">
         <Calculator />
       </div>
     );
